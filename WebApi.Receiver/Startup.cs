d6b763a0ec4bf0d2f81f31bae71e4e7bd53b9f71
Rafael B Machado <rafaeobm@hotmail.comm>
2020-04-03 00:29:32 -0300

    Add original AddMultitenancy method from Saaskit

--- WebApi.Receiver/Startup.cs
+++ WebApi.Receiver/Startup.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using SaasKit.Multitenancy;
+using SaasKit.Multitenancy.Internal;
+using System;
 
 namespace WebApi.Receiver
 {
@@ -45,4 +50,36 @@ namespace WebApi.Receiver
             });
         }
     }
+
+    public static class MultitenancyServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMultitenancy<TTenant, TResolver>(this IServiceCollection services)
+            where TResolver : class, ITenantResolver<TTenant>
+            where TTenant : class, new()
+        {
+            Ensure.Argument.NotNull(services, nameof(services));
+
+            services.AddScoped<ITenantResolver<TTenant>, TResolver>();
+
+            // No longer registered by default as of ASP.NET Core RC2
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // Make Tenant and TenantContext injectable
+            services.AddScoped(prov => prov.GetService<IHttpContextAccessor>()?.HttpContext?.GetTenantContext<TTenant>());
+            services.AddScoped(prov => prov.GetService<TenantContext<TTenant>>()?.Tenant);
+
+            // Make ITenant injectable for handling null injection, similar to IOptions
+            services.AddScoped<ITenant<TTenant>>(prov => new TenantWrapper<TTenant>(prov.GetService<TTenant>()));
+
+            // Ensure caching is available for caching resolvers
+            var resolverType = typeof(TResolver);
+            if (typeof(MemoryCacheTenantResolver<TTenant>).IsAssignableFrom(resolverType))
+            {
+                services.AddMemoryCache();
+            }
+
+            return services;
+        }
+    }
+
 }
