122aa71803ab296d373c87bb5afde795236bcac3
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2020-04-03 00:22:53 -0300

    Add Ping and Pong messages

---
+++ Messages/Ping.cs
@@ -0,0 +1,11 @@
+﻿using NServiceBus;
+using System;
+
+namespace Messages
+{
+    public class Ping : IMessage
+    {
+        public string From { get; set; }
+        public DateTime DateTimeNow { get; } = DateTime.Now;
+    }
+}
