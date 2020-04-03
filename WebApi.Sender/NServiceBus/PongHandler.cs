71be8139bc24eb3aea804183685da123ccb90650
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2020-04-03 00:39:05 -0300

    Add PongHandler to handle reply from PingHandler

---
+++ WebApi.Sender/NServiceBus/PongHandler.cs
@@ -0,0 +1,17 @@
+﻿using Messages;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApi.Sender.NServiceBus
+{
+    public class PongHandler : IHandleMessages<Pong>
+    {
+        public Task Handle(Pong message, IMessageHandlerContext context)
+        {
+            Console.WriteLine("Pong recebido!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
